75c9ebbb1ce9c53817ef57f2ebd91dd806a637dc
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-08 19:30:02 -0800

    Add reactions to the thoughts query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,11 +4,19 @@ const {gql} = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
   type Thought {
-    _id: ID,
-    thoughtText: String,
-    createdAt: String,
-    username: String,
-    reactionCount: Int,
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
   }
 
   type Query {
