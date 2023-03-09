e721aaec57f2e1e2dd4cf1a555f8e728baa3d052
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-08 19:59:09 -0800

    Add login and sign up mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,49 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1 });
+            return await Thought.find(params).sort({ createdAt: -1 });
         },
         thought: async(parent, { _id }) => {
-            return Thought.findById({_id});
+            return await Thought.findById({_id});
         },
         users: async () => {
-            return User.find()
+            return await User.find()
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts')
                 .sort({username: 1});
         },
         user: async(parent, { username }) => {
-            return User.findOne({ username })
+            return await User.findOne({ username })
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            return user;
         },
     },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,6 +34,11 @@ const typeDefs = gql`
     users: [User]
     user(username: String!): User
   }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
+  }
 `;
 
 // export our typeDefs
