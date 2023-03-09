009185c024ad1fb877e0ce5e1228beff57544107
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-08 19:39:29 -0800

    Add queries to get one thought, all users, and a user by username

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,25 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
-        thoughts: async (parent, {username}) => {
+        thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async(parent, { _id }) => {
+            return Thought.findById({_id});
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+                .sort({username: 1});
+        },
+        user: async(parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
     },
 };
