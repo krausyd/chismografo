9e64d5a9cb2aa250e251fba5cea77ce089065868
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-12 19:53:16 -0700

    Add mutations for adding friends, adding thoughts and adding reactions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,6 +60,42 @@ const resolvers = {
 
             return {token, user};
         },
+        addThought: async (parent, args, context) => {
+            if (context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username});
+                await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push: { thoughts: thought._id }},
+                    {new: true},
+                );
+                return thought;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        addReaction: async (parent, {thoughtId, reactionBody}, context) => {
+            if (context.user) {
+                const updatedThought = await Thought.findOneAndUpdate(
+                  {_id: thoughtId},
+                  {$push: { reactions: {reactionBody: reactionBody, username: context.user.username}}},
+                  {new: true, runValidators: true},  
+                );
+
+                return updatedThought;
+            }
+            throw new AuthenticationError('Not logged in');   
+        },
+        addFriends: async (parent, {friendId}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {friends: friendId}},
+                    {new: true}
+                ).populate('friends');
+
+                return updatedUser;
+            }
+            throw new AuthenticationError('Not logged in');   
+        },
     },
 };
 
