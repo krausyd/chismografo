b04a1cad6437494fdac6f6f8c627f816777459b8
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-12 20:29:22 -0700

    Add query to get all thoughts and a component to display in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtsList from '../components/ThoughtsList';
 
 const Home = () => {
+  const {loading, data} = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>
+          { loading ? (
+            <div>Loading ... </div>
+          ) : (
+            <ThoughtsList thoughts={thoughts} title="Some Feed for thoughts..." />
+          )}
+        </div>
       </div>
     </main>
   );
